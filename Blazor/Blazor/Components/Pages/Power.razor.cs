a448ef4e96e47437a9bc2d6f42d844e1bff48e7b
Ada <Ada@PROBOOK>
2026-08-29 19:21:04 +0700

    power done

---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		double num = 0;
+		int deg = 1;
+		double pow = 0;
+		int Deg
+		{
+			get => deg;
+			set => deg = value < 0 ? 0 : value > 50 ? 50 : value;
+		}
+		void CalculatePow()
+		{
+			pow = Math.Pow(num, deg);
+		}
+	}
+}
